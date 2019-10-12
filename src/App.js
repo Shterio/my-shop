db2384556923df8abf26e3cdd46ee0eae6defd88
Shterio <Shterio@users.noreply.github.com>
2019-10-12 20:11:25 +0200

    new components adding

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Switch, Route } from "react-router-dom";
-import ShopPage from "./pages/shop/shop";
-import Header from './components/header/header';
+import ShopPage from "./pages/shop/shop.component";
+import Header from './components/header/header.component';
 
 import HomePage from './pages/homepage/homepage.component';
+import SignIn from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+
 
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
-    <Header/>
-    <Switch>
-      <Route exact path='/' component={HomePage} />
-      <Route exact path='/shop' component={ShopPage} />
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signin' component={SignIn} />
       </Switch>
     </div>
   );
